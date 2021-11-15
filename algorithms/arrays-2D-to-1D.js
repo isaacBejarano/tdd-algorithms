4b373f04827281e15029b6edb81cf0e69b77d914
Isaac Bejarano <44377797+isaacBejarano@users.noreply.github.com>
2021-11-15 14:54:14 +0100

    Quicksort (#1)
    * vector is of numbers: red
    
    * vector is of numbers: green
    
    * quicksort sorted the vector: red
    
    * quicksort sorted the vector: green
    
    * quicksort sorted the vector: red
    
    * quicksort sorted the vector: green
    
    * quicksort sorted the vector: refactor
    
    * quicksort sorted the vector: refactor 2
    
    * quicksort sorted the vector complex cases: red
    
    * quicksort sorted the vector complex cases: red refactor
    
    * quicksort sorted the vector complex cases: red refactor 2
    
    * quicksort sorted the vector complex cases: green
    
    * quicksort only sorts DESC: red
    
    * quicksort only sorts DESC: red refactor
    
    * quicksort only sorts DESC: green
    
    * arrays2DTo1D() requires a bidimensional array: red

---
+++ algorithms/arrays-2D-to-1D.js
@@ -0,0 +1,3 @@
+function arrays2DTo1D() {}
+
+module.exports = arrays2DTo1D;
---
+++ algorithms/quicksort.js
@@ -0,0 +1,24 @@
+function quickSortDESC(vector) {
+	let left = [];
+	let right = [];
+	let pivot = vector[0];
+	let output = vector; // else... def CASE BASE
+
+	// if... RECURSION
+	if (vector.length >= 2) {
+		// sort
+		for (let i = 1; i < vector.length; i++) {
+			if (vector[i] < pivot) left[left.length] = vector[i];
+			// push left
+			else right[right.length] = vector[i]; // push right
+		}
+
+		// and repeat
+		// output = [...quickSortDESC(left), pivot, ...quickSortDESC(right)]; // ASC
+		output = [...quickSortDESC(right), pivot, ...quickSortDESC(left)]; // DESC
+	}
+
+	return output;
+}
+
+module.exports = quickSortDESC;
