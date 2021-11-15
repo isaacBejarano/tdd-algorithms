9e2dcdec506570f4021cbb2ed2ac4fd1c114eb7e
isaacBejarano <isaacbejaranopro@gmail.com>
2021-11-15 14:19:25 +0100

    vector is of numbers: green

--- tests/quicksort.spec.js
+++ tests/quicksort.spec.js
@@ -4,7 +4,13 @@ let vector = []; // numbers[]
 const quickSort = require("../algorithms/quicksort");
 
 describe("new hundredDoors()", () => {
+	beforeEach(() => {
+		vector = [5, 2, 0, 1, 9, 8, 3, 7, 4, 6]; // unordered
+	});
 
+	afterEach(() => {
+		vector = [5, 2, 0, 1, 9, 8, 3, 7, 4, 6]; // unordered
+	});
 
 	it("vector is of type sort number[]", () => {
 		expect(vector).toEqual(expect.arrayContaining([expect.any(Number)]));
