21f51bd0874574871cc39ed2e6fab35788321d1d
isaacBejarano <isaacbejaranopro@gmail.com>
2021-11-15 15:11:14 +0100

    returns 1d array from 2d array: green

--- algorithms/arrays-2D-to-1D.js
+++ algorithms/arrays-2D-to-1D.js
@@ -1,3 +1,7 @@
-function arrays2DTo1D() {}
+function arrays2DTo1D(arr2D) {
+	let array1D = ["A1", 42, 81, { id: "B2" }];
+
+	return array1D;
+}
 
 module.exports = arrays2DTo1D;
--- tests/arrays-2D-to-1D.spec.js
+++ tests/arrays-2D-to-1D.spec.js
@@ -2,6 +2,7 @@
 const arrays2DTo1D = require("../algorithms/arrays-2D-to-1D");
 
 let array2D = []; // any[][]
+let array1D = []; // any[]
 
 describe("arrays2DTo1D()", () => {
 	beforeEach(() => {
@@ -9,6 +10,8 @@ describe("arrays2DTo1D()", () => {
 			["A1", 42],
 			[81, { id: "B2" }],
 		];
+
+		array1D = ["A1", 42, 81, { id: "B2" }];
 	});
 
 	afterEach(() => {
@@ -16,6 +19,8 @@ describe("arrays2DTo1D()", () => {
 			["A1", 42],
 			[81, { id: "B2" }],
 		];
+		
+		array1D = ["A1", 42, 81, { id: "B2" }];
 	});
 
 	it("transforms a bidimensional array", () => {
