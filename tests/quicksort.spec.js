87399f19e5d624a1e56dd5c5df5cf73129ac56fb
isaacBejarano <isaacbejaranopro@gmail.com>
2021-11-15 14:18:38 +0100

    vector is of numbers: red

--- tests/quicksort.spec.js
+++ tests/quicksort.spec.js
@@ -4,20 +4,13 @@ let vector = []; // numbers[]
 const quickSort = require("../algorithms/quicksort");
 
 describe("new hundredDoors()", () => {
-	beforeEach(() => {
-		vector = [5, 2, 0, 1, 9, 8, 3, 7, 4, 6];
-	});
 
-	afterEach(() => {
-		vector = [5, 2, 0, 1, 9, 8, 3, 7, 4, 6];
-	});
 
 	it("vector is of type sort number[]", () => {
+		expect(vector).toEqual(expect.arrayContaining([expect.any(Number)]));
 	});
 
-	it("quicksort ordered the vector", () => {
-	});
+	it("quicksort ordered the vector", () => {});
 
-	it("quicksort can order descendently", () => {
-	});
+	it("quicksort can order descendently", () => {});
 });
