fb144caca95e1e0ea27a652de5c2e26a5c2d8624
isaacBejarano <isaacbejaranopro@gmail.com>
2021-11-15 15:02:42 +0100

    array 2D required: red

--- tests/arrays-2D-to-1D.spec.js
+++ tests/arrays-2D-to-1D.spec.js
@@ -1,12 +1,12 @@
 "use strict";
 const arrays2DTo1D = require("../algorithms/arrays-2D-to-1D");
 
+let array2D = []; // any[][]
 
 describe("arrays2DTo1D()", () => {
-	
-	it("transforms a bidimensional array", () => {
-		expect(array2D).toEqual([['A1','A2'],['B1','B2']]);
-	});
-	
 
+
+	it("transforms a bidimensional array", () => {
+		expect(array2D).toEqual(expect.arrayContaining([expect.arrayContaining([expect.anything()])]));
+	});
 });
