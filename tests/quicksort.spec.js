f86015dd614665b929b89e84fa83c349d1ebfbfe
isaacBejarano <isaacbejaranopro@gmail.com>
2021-11-15 14:13:37 +0100

    init and gitignore

---
+++ tests/quicksort.spec.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let vector = []; // numbers[]
+const quickSort = require("../algorithms/quicksort");
+
+describe("new hundredDoors()", () => {
+	beforeEach(() => {
+		vector = [5, 2, 0, 1, 9, 8, 3, 7, 4, 6];
+	});
+
+	afterEach(() => {
+		vector = [5, 2, 0, 1, 9, 8, 3, 7, 4, 6];
+	});
+
+	it("vector is of type sort number[]", () => {
+	});
+
+	it("quicksort ordered the vector", () => {
+	});
+
+	it("quicksort can order descendently", () => {
+	});
+});
