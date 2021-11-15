8ea56bfe201eb771226ab05f2d9f21746b16c0a1
isaacBejarano <isaacbejaranopro@gmail.com>
2021-11-15 14:22:41 +0100

    quicksort sorted the vector: red

--- tests/quicksort.spec.js
+++ tests/quicksort.spec.js
@@ -1,22 +1,27 @@
 "use strict";
-
-let vector = []; // numbers[]
 const quickSort = require("../algorithms/quicksort");
+
+let vector = []; // numbers[] unsorted
+let desc = []; // numbers[] sorted
 
 describe("new hundredDoors()", () => {
 	beforeEach(() => {
-		vector = [5, 2, 0, 1, 9, 8, 3, 7, 4, 6]; // unordered
+		vector = [5, 2, 0, 1, 9, 8, 3, 7, 4, 6]; // unsorted
+		desc = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]; // sorted
 	});
 
 	afterEach(() => {
-		vector = [5, 2, 0, 1, 9, 8, 3, 7, 4, 6]; // unordered
+		vector = [5, 2, 0, 1, 9, 8, 3, 7, 4, 6]; // unsorted
+		desc = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]; // sorted
 	});
 
 	it("vector is of type sort number[]", () => {
 		expect(vector).toEqual(expect.arrayContaining([expect.any(Number)]));
 	});
 
-	it("quicksort ordered the vector", () => {});
+	it("quicksort sorted the vector", () => {
+		expect(quickSort).toEqual(desc);
+	});
 
 	it("quicksort can order descendently", () => {});
 });
