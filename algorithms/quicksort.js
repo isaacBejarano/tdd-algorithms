0b1d338f1fe198b41cd3f7f3a25cd6b9ee7bf536
isaacBejarano <isaacbejaranopro@gmail.com>
2021-11-15 14:41:53 +0100

    quicksort sorted the vector complex cases: green

--- algorithms/quicksort.js
+++ algorithms/quicksort.js
@@ -6,9 +6,16 @@ function quickSortDESC(vector) {
 
 	// if... RECURSION
 	if (vector.length >= 2) {
-		
-	}
+		// sort
+		for (let i = 1; i < vector.length; i++) {
+			if (vector[i] < pivot) left[left.length] = vector[i];
+			// push left
+			else right[right.length] = vector[i]; // push right
+		}
 
+		// and repeat
+		output = [...quickSortDESC(left), pivot, ...quickSortDESC(right)]; // ASC
+	}
 
 	return output;
 }
--- tests/quicksort.spec.js
+++ tests/quicksort.spec.js
@@ -2,16 +2,19 @@
 const quickSortDESC = require("../algorithms/quicksort");
 
 let vector = []; // numbers[] unsorted
+let asc = []; // numbers[] sorted
 let desc = []; // numbers[] sorted
 
 describe("quickSortDESC()", () => {
 	beforeEach(() => {
 		vector = [5, 2, 0, 1, 9, 8, 3, 7, 4, 6]; // unsorted
+		asc = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; // sorted
 		desc = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]; // sorted
 	});
 
 	afterEach(() => {
 		vector = [5, 2, 0, 1, 9, 8, 3, 7, 4, 6]; // unsorted
+		asc = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; // sorted
 		desc = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]; // sorted
 	});
 
@@ -24,7 +27,7 @@ describe("quickSortDESC()", () => {
 	});
 
 	it("quickSortDESC sorted the vector: COMPLEX CASES recursively", () => {
-		expect(quickSortDESC(vector)).toEqual(desc);
+		expect(quickSortDESC(vector)).toEqual(asc);
 	});
 
 	it("quickSortDESC only orders descendently", () => {});
